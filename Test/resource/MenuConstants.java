0d5637baf958bea24932d9ea0c9e5dfba7c53ab6
Brayn <Brayn.lutz@hotmail.com>
2022-01-05 17:10:54 +0100

    Update

--- Test/resource/MenuConstants.java
+++
@@ -1,9 +0,0 @@
-package ch.iso.m266b.resource;
-
-public class MenuConstants {
-	
-	public static final String MENU_TOOLS = "Tools";
-	public static final String MENU_TOOLS_DEL = "Daten löschen";
-	public static final String MENU_TOOLS_EXIT = "Programm Verlassen";
-
-}
